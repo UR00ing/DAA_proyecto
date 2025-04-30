7701145ef1d83f8f9a45826f627909b4aa1e8ce3
UR00ing <jvergarat1800@alumno.ipn.mx>
2025-04-30 13:07:25 -0600

    Proyecto 2 terminado

--- Proyecto.py
+++ Proyecto.py
@@ -1,42 +1,91 @@
 import	graph_lib	as	gp
 import	algoritmos	as	alg
+import	pathlib		as	pl
 
-#	50 nodos
-grMalla		=	alg.MetodoMalla(8,7)
-grMalla.GraphViz('1Malla,8,7')
-grEyR		=	alg.MetodoErdosyRenyi(50,75)
-grEyR.GraphViz('1Erdos,50,75')
-Gilbert		=	alg.MetodoGilbert(50,0.15)
-Gilbert.GraphViz('1Gilbert,50,0_15')
-GeoGraph	=	alg.MetodoGeoSimp(50,0.25)
-GeoGraph.GraphViz('1Geografico,50,0_25')
-Barabasi	=	alg.MetodoBarabasiAlbert(50,3)
-Barabasi.GraphViz('1Barabasi,50,3')
-Dorogovtsev	=	alg.MetodoDorogovtsev(50)
-Dorogovtsev.GraphViz('1Dorogovtsev,50')
-#	200 nodos
-grMalla		=	alg.MetodoMalla(18,12)
-grMalla.GraphViz('2Malla,18,12')
-grEyR		=	alg.MetodoErdosyRenyi(200,250)
-grEyR.GraphViz('2Erdos,200,250')
-Gilbert		=	alg.MetodoGilbert(200,0.1)
-Gilbert.GraphViz('2Gilbert,200,0_1')
-GeoGraph	=	alg.MetodoGeoSimp(200,0.3)
-GeoGraph.GraphViz('2Geografico,200,0_3')
-Barabasi	=	alg.MetodoBarabasiAlbert(200,4)
-Barabasi.GraphViz('2Barabasi,200,4')
-Dorogovtsev	=	alg.MetodoDorogovtsev(200)
-Dorogovtsev.GraphViz('2Dorogovtsev,200')
+#	Directorios a usar
+Dir			=	pl.Path(__file__).parent.absolute()
+Dir			=	Dir / "proyecto_2"
+DirS		=	Dir	/	"SaveGraph"
+DirBFS		=	Dir	/	"SaveGraph/BFS"
+DirDFSi		=	Dir	/	"SaveGraph/DFSi"
+DirDFSr		=	Dir	/	"SaveGraph/DFSr"
+
+"""
+Funcion para obtener los nombres de los archivos en un directorio
+"""
+def	List_files_on(addr):
+	lista_nombres = []
+	for archivo in pl.Path(addr).iterdir():
+		if archivo.is_file() and archivo.suffix == '.gv':
+			lista_nombres.append(archivo.stem)  # .stem elimina automáticamente la extensión
+	return	lista_nombres
+
+"""
+Para proyecto 1
+se crean los grafos con los metodos de generacion
+"""
+#	30 nodos
+grMalla		=	alg.MetodoMalla(6,5)
+grMalla.GraphViz('1Malla,6,5',DirS)
+grEyR		=	alg.MetodoErdosyRenyi(30,75)
+grEyR.GraphViz('1Erdos,30,75',DirS)
+Gilbert		=	alg.MetodoGilbert(30,0.5)
+Gilbert.GraphViz('1Gilbert,30,0_5',DirS)
+GeoGraph	=	alg.MetodoGeoSimp(30,0.40)
+GeoGraph.GraphViz('1Geografico,30,0_40',DirS)
+Barabasi	=	alg.MetodoBarabasiAlbert(30,4)
+Barabasi.GraphViz('1Barabasi,30,4',DirS)
+Dorogovtsev	=	alg.MetodoDorogovtsev(30)
+Dorogovtsev.GraphViz('1Dorogovtsev,30',DirS)
+#	100 nodos
+grMalla		=	alg.MetodoMalla(10,10)
+grMalla.GraphViz('2Malla,10,10',DirS)
+grEyR		=	alg.MetodoErdosyRenyi(100,250)
+grEyR.GraphViz('2Erdos,100,250',DirS)
+Gilbert		=	alg.MetodoGilbert(100,0.3)
+Gilbert.GraphViz('2Gilbert,100,0_3',DirS)
+GeoGraph	=	alg.MetodoGeoSimp(100,0.3)
+GeoGraph.GraphViz('2Geografico,100,0_3',DirS)
+Barabasi	=	alg.MetodoBarabasiAlbert(100,4)
+Barabasi.GraphViz('2Barabasi,100,4',DirS)
+Dorogovtsev	=	alg.MetodoDorogovtsev(100)
+Dorogovtsev.GraphViz('2Dorogovtsev,100',DirS)
 #	500 nodos
 grMalla		=	alg.MetodoMalla(29,18)
-grMalla.GraphViz('3Malla,29,18')
-grEyR		=	alg.MetodoErdosyRenyi(500,750)
-grEyR.GraphViz('3Erdos,500,750')
+grMalla.GraphViz('3Malla,29,18',DirS)
+grEyR		=	alg.MetodoErdosyRenyi(500,1300)
+grEyR.GraphViz('3Erdos,500,1300',DirS)
 Gilbert		=	alg.MetodoGilbert(500,0.15)
-Gilbert.GraphViz('3Gilbert,500,0_15')
+Gilbert.GraphViz('3Gilbert,500,0_15',DirS)
 GeoGraph	=	alg.MetodoGeoSimp(500,0.25)
-GeoGraph.GraphViz('3Geografico,500,0_25')
+GeoGraph.GraphViz('3Geografico,500,0_25',DirS)
 Barabasi	=	alg.MetodoBarabasiAlbert(500,5)
-Barabasi.GraphViz('3Barabasi,500,5')
+Barabasi.GraphViz('3Barabasi,500,5',DirS)
 Dorogovtsev	=	alg.MetodoDorogovtsev(500)
-Dorogovtsev.GraphViz('3Dorogovtsev,500')
+Dorogovtsev.GraphViz('3Dorogovtsev,500',DirS)
+
+"""
+Para proyecto 2
+se leen los grafos y se crean los arboles con los 
+metodos BFS y DFS(para metodo iterativo y recursivo)
+"""
+
+#	Lectura de los archivos
+grafos	=	[]
+names	=	List_files_on(DirS)
+for	l	in	names:
+	grafos.append(gp.Graph(l).load_graph(l,DirS))
+TempOrder	=	[]
+GRtemp		=	gp.Graph()
+#	Creacion de arboles en BFS
+for i in range(len(names)):
+	TempOrder,GRtemp	=	grafos[i].BFS()
+	GRtemp.GraphViz(names[i].replace('\n', ''),DirBFS)
+#	Creacion de arboles en DFS iterativo
+for i in range(len(names)):
+	TempOrder,GRtemp	=	grafos[i].DFS_i()
+	GRtemp.GraphViz(names[i].replace('\n', ''),DirDFSi)
+#	Creacion de arboles en DFS recursivo
+for i in range(len(names)):
+	TempOrder,GRtemp	=	grafos[i].DFS_r()
+	GRtemp.GraphViz(names[i].replace('\n', ''),DirDFSr)
--- graph_lib.py
+++ graph_lib.py
@@ -1,6 +1,7 @@
 import	random	as rd
 import	numpy	as np
-import pathlib	as pl
+import	pathlib	as pl
+import	copy	as cp
 
 EDGE = 'edge'
 NEIG = 'neighbor'
@@ -28,17 +29,47 @@ class	Edge:
 class	Graph:
 	"	GRAFO	"
 
-	def	__init__(self,name):
-		self.NAME	=	name	+	"_GRAPH"
+	def	__init__(self,name="GR"):
+		self.NAME	=	str(name)	+	"_GRAPH"
 		self.NODES	=	{}
 		self.EDGES	=	{}
 	
+	def	change_name(self,newName):
+		"""
+		Cambia el nombre identificador del grafo
+		"""
+		self.NAME	=	newName
+	
+	def	get_name(self):
+		"""
+		Entrega el nombre identificador del grafo
+		"""
+		return	self.NAME
+	
+	def	get_nodes(self):
+		"""
+		Entrega el diccionario de nodos del grafo
+		"""
+		return	self.NODES
+	
+	def	get_edges(self):
+		"""
+		Entrega el diccionario de aristas del grafo
+		"""
+		return	self.EDGES
+
 	def addNode(self,id):
+		"""
+		Agrega un nodo al grafo
+		"""
 		if id not in self.NODES:
 			self.NODES[str(id)]	=	Node(str(id))
 		return	self.NODES[str(id)]
 	
 	def	addEdge(self,id,nodeSource,nodeTarget):
+		"""
+		Agrega una arista al grafo
+		"""
 		if id not in self.EDGES:
 			n0				=	self.addNode(str(nodeSource))
 			n1				=	self.addNode(str(nodeTarget))
@@ -55,10 +86,13 @@ class	Graph:
 
 		return edge
 	
-	def	GraphViz(self,GVdoc):
+	def	GraphViz(self,GVdoc,GVdir="SaveGraph"):
+		"""
+		crea el archivo GV para gephi
+		"""
 		# Obtener el directorio actual del script
 		current_dir	=	pl.Path(__file__).parent.absolute()
-		save_dir	=	current_dir / "SaveGraph"
+		save_dir	=	current_dir / GVdir
 		# Crear la carpeta si no existe
 		save_dir.mkdir(exist_ok=True)
 		# Crear la ruta completa del archivo
@@ -86,13 +120,13 @@ class	Graph:
 		"""
 		Verifica si un nodo existe en el grafo
 		"""
-		return node in self.NODES
+		return node in self.NODES.values()
 	
 	def edge_exist(self, edge):
 		"""
 		Verifica si una arista existe en el grafo
 		"""
-		return edge in self.EDGES
+		return edge in self.EDGES.values()
 	
 	def get_node_pos(self, node):
 		"""
@@ -116,7 +150,202 @@ class	Graph:
 		"""
 		return	rd.choice(list(self.EDGES.values()))
 	
-	def	get_node_from_edge(self,edge):
+	def	get_random_node(self):
 		"""
-		regresa los nodos conectados a una arista
+		busca y entrega aleatoriamente un nodo
 		"""
+		return	rd.choice(list(self.NODES.values()))
+
+	def	clone_Graph(self,name="self"):
+		r	=	Graph()
+
+		if	name	==	"self":
+			r.NAME	=	self.NAME	+	"_Clone"
+		else:
+			r.NAME	=	str(name)
+		r.NODES	=	self.NODES.copy()
+		r.EDGES	=	self.EDGES.copy()
+
+		return	r
+	
+	def	get_nodes_of_edge(self,edge):
+		"""
+		Entrega los nodos conectados a una arista 
+		en formato de lista 
+		"""
+		nodes	=	[]
+		if	self.edge_exist(edge):
+			nodes.append(edge.SOURCE)
+			nodes.append(edge.TARGET)
+			return nodes
+		return	None
+	
+	@staticmethod
+	def	load_graph(FileName,FileDir,GraphName=None):
+		"""
+		Lee un archivo .gv y lo convierte en un grafo
+		de esta clase.
+		"""
+		# Obtener el directorio actual del script
+		current_dir	=	pl.Path(__file__).parent.absolute()
+		save_dir	=	current_dir / FileDir
+		# Crear la carpeta si no existe
+		save_dir.mkdir(exist_ok=True)
+		# Crear la ruta completa del archivo
+		file_dir	=	save_dir / f"{FileName}.gv"
+		print("Creando grafo   ...   ...")
+		if	GraphName	==	None:
+			Gret	=	Graph(str(FileName))
+		else:
+			Gret	=	Graph(str(GraphName))
+		f	=	open(file_dir,'r')
+		lines	=	f.readlines()
+		for i in range(1,len(lines)-1):
+			line	=	lines[i].replace('\n', '')
+			line	=	line.replace(';', '')
+			elements	=	line.split(' ')
+			if len(elements)	==	3:
+				Gret.addEdge('e'+str(i),elements[0],elements[2])
+			else:
+				Gret.addNode(elements[0])
+		return	Gret
+		
+	def DFS_i(self, start_node=None):
+		"""
+		Realiza un recorrido DFS de modo iterativo y 
+		genera el árbol DFS correspondiente 
+		"""
+		#	Se selecciona el nodo de inicio
+		if start_node is None:	
+			#	Si no se entrega un nodo se toma uno random
+			str_n = self.get_random_node().id
+		else:
+			str_n = str(start_node.id)
+			if str_n not in self.NODES:
+				#	Si se entrega un nodo valido se crea uno random
+				print("El nodo dado no existe en el grafo, se tomará uno aleatoriamente")
+				str_n = self.get_random_node().id
+
+		#	Se crean las listas para uso en la busqueda.
+		visited = set()
+		stack = [(str_n, None)]  # (nodo_actual, nodo_raiz)
+		visit_order = []
+		#	Se crea un nuevo grafo para el árbol
+		dfs_tree = Graph(self.NAME	+	"_DFS_Tree_iterativo")  
+
+		#	Se inicia la busqueda a lo ancho
+		i	=	0	#	Contador para las aristas
+		while stack:
+			node_id, parent_id = stack.pop()
+			if node_id not in visited:
+				visited.add(node_id)
+				visit_order.append(node_id)
+				dfs_tree.addNode(node_id)  # Agregar el nodo al árbol
+
+				# Registrar la arista en el árbol (si no es el nodo raíz)
+				if parent_id is not None:
+					i	=	i	+	1
+					edge_id = "e"	+	str(i)
+					dfs_tree.addEdge(edge_id, parent_id, node_id)
+
+				# Apilar vecinos no visitados (en orden inverso para mantener el orden natural)
+				neighbors = [neighbor.id for neighbor in self.NODES[node_id].attr[NEIG]]
+				for neighbor in reversed(neighbors):
+					if neighbor not in visited:
+						stack.append((neighbor, node_id))
+
+		return visit_order, dfs_tree
+	
+	def DFS_r(self, start_node=None):
+		"""
+		Realiza un recorrido DFS de modo recursivo y 
+		genera el árbol DFS correspondiente 
+	    """
+		#	Se selecciona el nodo de inicio
+		if start_node is None:	
+			#	Si no se entrega un nodo se toma uno random
+			str_n = self.get_random_node().id
+		else:
+			str_n = str(start_node.id)
+			if str_n not in self.NODES:
+				#	Si se entrega un nodo valido se crea uno random
+				print("El nodo dado no existe en el grafo, se tomará uno aleatoriamente")
+				str_n = self.get_random_node().id
+			
+		#	Se crean las listas para uso en la busqueda.
+		visited = set()
+		visit_order = []
+		#	Se crea un nuevo grafo para el árbol
+		dfs_tree = Graph(self.NAME	+	"_DFS_Tree_recursivo")  	
+		#	Función auxiliar recursiva
+		def _dfs_helper(node_id, parent_id=None):
+			"""
+		    Función recursiva que realiza el recorrido DFS y construir el árbol
+		    """
+			#	Marcar nodo como visitado y registrar en estructuras
+			visited.add(node_id)
+			visit_order.append(node_id)
+			dfs_tree.addNode(node_id)  #	Agregar nodo al árbol DFS
+
+			#	Si hay nodo raiz, crear arista en el árbol DFS
+			if parent_id is not None:
+				edge_id = "DFS_"	+	str(parent_id)	+	"_"	+	str(node_id)
+				dfs_tree.addEdge(edge_id, parent_id, node_id)		
+		    #	Explorar todos los vecinos del nodo actual
+			for neighbor in self.NODES[node_id].attr[NEIG]:
+				if neighbor.id not in visited:
+					#	Llamada recursiva para vecinos no visitados
+					_dfs_helper(neighbor.id, node_id)	#	El nodo actual será la raiz en la recursión
+		#	Iniciar el recorrido DFS desde el nodo inicial
+		_dfs_helper(str_n)
+		return visit_order, dfs_tree
+	
+	def BFS(self, start_node=None):
+		"""Realiza un recorrido BFS y genera el árbol DFS correspondiente 
+		"""
+		#	Se selecciona el nodo de inicio
+		if start_node is None:	
+			#	Si no se entrega un nodo se toma uno random
+			str_n = self.get_random_node().id
+		else:
+			str_n = str(start_node.id)
+			if str_n not in self.NODES:
+				#	Si se entrega un nodo valido se crea uno random
+				print("El nodo dado no existe en el grafo, se tomará uno aleatoriamente")
+				str_n = self.get_random_node().id
+
+		#	Se crean las listas para uso en la busqueda.
+		visited = set()
+		queue = [(str_n, None)]	#	(nodo_actual, nodo_raiz)
+		visit_order = []
+		#	Se crea un nuevo grafo para el árbol
+		bfs_tree = Graph(self.NAME	+	"_BFS_Tree")
+
+		#	Se inicia la busqueda a lo ancho
+		edge_counter = 0	#	Contador para las aristas
+		while queue:
+			#	Extraer el primer nodo de la cola
+			node_id, parent_id = queue.pop(0)
+			
+			if node_id not in visited:
+				#	Procesar nodo actual
+				visited.add(node_id)
+				visit_order.append(node_id)
+				bfs_tree.addNode(node_id)  # Agregar nodo al árbol
+
+				#	Registrar arista en el árbol (excepto para raíz)
+				if parent_id is not None:
+					edge_counter += 1
+					edge_id = "e"	+	str(edge_counter)
+					bfs_tree.addEdge(edge_id, parent_id, node_id)
+
+				#	Encolar vecinos no visitados de manera alfabetica (sorted es para ordenarlo)
+				neighbors = sorted(
+					[neighbor.id for neighbor in self.NODES[node_id].attr[NEIG]],
+					key=lambda x: x  # Orden alfabético/númerico
+				)
+				for neighbor in neighbors:
+					if neighbor not in visited:
+						queue.append((neighbor, node_id))
+
+		return visit_order, bfs_tree
